6d31344cf9d10b3bbfe161c4958bf09d974957f8
Autumn60 <r117159@g.kobe-kosen.ac.jp>
2023-11-23 18:07:39 +0900

    Add example

---
+++ examples/example.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include <RobomasterMotorHandler.h>
+#include <RobomasterMotor.h>
+
+using robomaster_motor_lib;
+
+RobomasterMotorHandler handler(&Can0);
+RobomasterMotor motor(1);
+
+void setup()
+{
+    Serial.begin(9600);
+    handler.AddMotor(&motor);
+}
+
+void loop()
+{
+    handler.Update();
+
+    motor.SetTargetCurrent(1.0);
+    
+    Serial.print("Raw Angle: ");
+    Serial.println(motor.GetRawAngle());
+    Serial.print("Incremental Angle: ");
+    Serial.println(motor.GetIncrementalAngle());
+    Serial.print("Velocity: ");
+    Serial.println(motor.GetVelocity());
+    Serial.print("Current: ");
+    Serial.println(motor.GetCurrent());
+}
